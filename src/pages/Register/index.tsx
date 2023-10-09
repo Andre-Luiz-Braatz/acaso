1650ad2a3a8c170d9734829ca8095bb167030a7e
André Braatz <andreluiz.braatz@gmail.com>
2023-10-09 19:58:51 -0300

    feat: assets screen auth/register

--- src/pages/Register/index.tsx
+++ src/pages/Register/index.tsx
@@ -1,3 +1,19 @@
+import anelPNG from "../../assets/img/anel.png";
+import glassPNG from "../../assets/img/gas.png";
+import logoPNG from "../../assets/img/logo.png";
+
 export default function Register() {
-  return <div className={"full centralizeC"}>acaso</div>;
+  return (
+    <div
+      className={"full centralizeC"}
+      style={{ background: `url(${anelPNG}) no-repeat right top fixed` }}
+    >
+      <div
+        className={"full centralizeC"}
+        style={{ background: `url(${glassPNG}) no-repeat right center fixed` }}
+      >
+        <img alt={"logo"} src={logoPNG} />
+      </div>
+    </div>
+  );
 }
